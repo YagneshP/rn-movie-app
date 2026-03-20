736a3f064ff60fee053dd25a12ece298a0ab8f63
yagnesh parekh <yagneshparekh@rocketmail.com>
2026-03-20 11:58:03 -0400

    Add Searchbar component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
 import { icons } from "@rn_movie_app/constants/icons";
 import { images } from "@rn_movie_app/constants/images";
+import { useRouter } from "expo-router";
 import { Image, ScrollView, View } from "react-native";
 import Searchbar from "../component/Searchbar";
-
 export default function Index() {
+  const router = useRouter();
   return (
     <View className='flex-1 bg-primary'>
       <Image source={images.bg} className='absolute w-full z-0' />
@@ -16,7 +17,10 @@ export default function Index() {
         }}
       >
         <Image source={icons.logo} className='w-12 h-10 mt-20 mb-5 mx-auto' />
-        <Searchbar />
+        <Searchbar
+          onPress={() => router.push("/search")}
+          placeholder='Search a Movie'
+        />
       </ScrollView>
     </View>
   );
--- app/component/Searchbar.tsx
+++ app/component/Searchbar.tsx
@@ -1,10 +1,28 @@
+import { icons } from "@rn_movie_app/constants/icons";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, TextInput, View } from "react-native";
+interface Props {
+  placeholder: "string";
+  onPress?: () => void;
+}
 
-const Searchbar = () => {
+const Searchbar = ({ onPress, placeholder }: Props) => {
   return (
-    <View>
-      <Text>Searchbar</Text>
+    <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
+      <Image
+        source={icons.search}
+        className='size-5'
+        resizeMode='contain'
+        tintColor='ab8bff'
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value=''
+        onChangeText={() => {}}
+        className='flex-1 ml-2 text-white'
+        placeholderTextColor='#A8B5DB'
+      />
     </View>
   );
 };
